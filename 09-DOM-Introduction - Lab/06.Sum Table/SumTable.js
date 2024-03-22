bef450e4c5920bfaaed506246b4244e7289ee66b
Amxx79 <e.siwanov@gmail.com>
2024-03-22 13:16:44 +0200

    DOM Introduction

--- 09-DOM-Introduction - Lab/06.Sum Table/SumTable.js
+++ 09-DOM-Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+    const tdElements = document.querySelectorAll('tr td:last-of-type:not(#sum)');
+    const sum = document.getElementById('sum');
 
+    const data = Array.from(tdElements)
+        .reduce((acc, el) => acc + Number(el.textContent), 0);
+        console.log(data);
+
+    sum.textContent = data;
 }
