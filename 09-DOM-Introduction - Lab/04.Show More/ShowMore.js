afa01cea4e6f9227e43a3c84654fa592b27eba8b
Amxx79 <e.siwanov@gmail.com>
2024-03-22 13:16:13 +0200

    DOM Introduction

--- 09-DOM-Introduction - Lab/04.Show More/ShowMore.js
+++ 09-DOM-Introduction - Lab/04.Show More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
     // TODO
+    const moreButton = document.getElementById('more');
+    const showText = document.getElementById('text');
+    showText.style.display = 'inline';
+    moreButton.style.display = 'none';
 }
